4d470975e9ee512258d84053506f79fbee98b22f
Kaisa <45688258+KooEeVee@users.noreply.github.com>
2022-11-20 08:50:33 +0200

    db security enhancement

--- users.py
+++ users.py
@@ -1,9 +1,12 @@
 from db import db
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 def register(username, password):
+    h_password = generate_password_hash(password)
     try:
         sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-        db.session.execute(sql, {"username":username, "password":password})
+        db.session.execute(sql, {"username":username, "password":h_password})
         db.session.commit()
     except:
         return False
